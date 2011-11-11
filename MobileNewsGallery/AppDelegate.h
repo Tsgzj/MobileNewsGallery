aafea97f476d95026b1accaa47ab18b0e392666e
Zongxuan Su <suzongxuan@gmail.com>
2011-11-11 23:44:00 +0800

    Initial Commit

---
+++ MobileNewsGallery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MobileNewsGallery
+//
+//  Created by Tommy Su on 11-11-11.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
